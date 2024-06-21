0538f82848a12807df77b3e0c3e7bd6a6cf8f048
taka-4602 <116678390+taka-4602@users.noreply.github.com>
2024-06-22 03:45:21 +0900

    Add files via upload

---
+++ archive/v4/v4path.py
@@ -0,0 +1,10 @@
+BOTTOKEN="Botのトークンをここに"
+ipath="userdata.json のパスをここに"
+ipath2="サーバーID.json のフォルダーパスをここに"
+authurl="作ったURLをここに"
+nowjson="now.json"#now.jsonのパスをここに
+
+API_ENDPOINT = 'https://discord.com/api/v10'
+CLIENT_ID = ['OAuth2のClientIDをここに']
+CLIENT_SECRET = ['OAuth2のClientSecretをここに']
+REDIRECT_URI = 'Flaskサーバーが建てられている場所(IPアドレスやドメイン)をここに'
---
+++ v6/v6path.py
@@ -0,0 +1,8 @@
+BOTTOKEN = "Botのトークンをここに"
+CLIENT_ID = "OAuth2のClientIDをここに"
+CLIENT_SECRET = "OAuth2のClientSecretをここに"
+REDIRECT_URI = "Flaskサーバーが建てられている場所(IPアドレスやドメイン)をここに"
+
+usadata_path = "userdata.json のパスをここに"
+serverdata_folder_path = "サーバーID.json のフォルダーパスをここに"
+authurl = "作ったURLをここに"
